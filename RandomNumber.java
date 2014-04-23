5215ee2b860fb3fbf827615c848e44c2fd65db4c
James Perez <xoptoss@gmail.com>
2014-04-22 21:15:11 -0700

    Create RandomNumber.java

---
+++ RandomNumber.java
@@ -0,0 +1,34 @@
+package mainPackage;
+
+import java.util.Random;
+
+public class RandomNumber {
+
+	int answer,high,low,newHigh,newLow;
+	Random random = new Random();
+	
+	public RandomNumber(int newHigh,int newLow)
+	{
+		high=newHigh;
+		low=newLow;
+	}
+	public int GetANumber()
+	{
+		answer = random.nextInt(high-low)+low;
+		return answer;	
+	}
+	public int setHighNumber(int newHigh)
+	{
+		high = newHigh;
+		return high;
+	}
+	public int GetHighNumber()
+	{
+		return high;
+	}
+	public int GetLowNumber()
+	{
+		return low;
+	}
+}
+
